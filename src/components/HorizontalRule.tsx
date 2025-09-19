60b5ad2b6ed6a0dda96239991c9a95525b4d6592
Ihab Awad <ihab.awad@gmail.com>
2025-09-19 15:30:52 -0700

    Bogo test PR.

---
+++ src/components/HorizontalRule.tsx
@@ -0,0 +1,9 @@
+import ruleWavy from '../assets/rule-wavy-wide.svg';
+
+const HorizontalRule = () => (
+  <div className="hr">
+    <img src={ruleWavy.src} alt="" />
+  </div>
+);
+
+export default HorizontalRule;
