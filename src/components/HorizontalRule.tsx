7853375bb8a474c6016375d60d285a90bf6d151d
Ihab Awad <ihab.awad@gmail.com>
2025-09-19 15:34:12 -0700

    Revert "Bogo test PR."
    This reverts commit 60b5ad2b6ed6a0dda96239991c9a95525b4d6592.

--- src/components/HorizontalRule.tsx
+++
@@ -1,9 +0,0 @@
-import ruleWavy from '../assets/rule-wavy-wide.svg';
-
-const HorizontalRule = () => (
-  <div className="hr">
-    <img src={ruleWavy.src} alt="" />
-  </div>
-);
-
-export default HorizontalRule;
