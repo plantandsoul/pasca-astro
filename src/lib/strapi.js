1dfb8e5a85f3e0ccd8f45e6bfab0e4d898df06b5
Culi T <76538249+tif-calin@users.noreply.github.com>
2025-09-27 09:20:35 -0700

    Add sitemap (#3)
    * feat: adds sitemap generator
    
    * fix: don't error when strapi isn't running
    
    * feat: adds robots.txt
    
    * feat: add sitemap link to head

--- src/lib/strapi.js
+++ src/lib/strapi.js
@@ -1,13 +1,18 @@
 const { STRAPI_URL, STRAPI_TOKEN } = import.meta.env;
 
 export async function fetchPosts() {
-  const response = await fetch(`${STRAPI_URL}/api/articles`, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${STRAPI_TOKEN}`,
-    }
-  });
-  const { data } = await response.json();
+  try {
+    const response = await fetch(`${STRAPI_URL}/api/articles`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${STRAPI_TOKEN}`,
+      }
+    });
+    const { data } = await response.json();
 
-  return data;
+    return data;
+  } catch (error) {
+    console.error('Error fetching posts from Strapi:', error);
+    return [];
+  }
 };
