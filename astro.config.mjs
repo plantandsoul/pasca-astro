a52a0902e02478c6e8f0e545232a4111063a2bd0
culi <culitif@tuta.io>
2025-09-26 17:50:46 -0700

    feat: adds netlify package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   integrations: [react(), sitemap()],
   site: 'https://pasca.org',
 });
